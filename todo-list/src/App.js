77df5a3c2e8b11de143e63b7307972dec915dff5
Bayu Ahmad Fauzan <bayu.ahmad.f@mail.ugm.ac.id>
2022-06-22 22:57:17 +0700

    update save to local storage

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -11,9 +11,16 @@ function App() {
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
 
+//
+useEffect(() => {
+  getLocalTodos();
+}, []);
+
+
 //use effect
 useEffect(() => {
   filterHandler();
+  saveLocalTodos();
 }, [todos, status]);
 
   //functions
@@ -29,7 +36,20 @@ useEffect(() => {
         setFilteredTodos(todos);
         break;
     }
-  }
+  };
+  //save to local
+  const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+
 
   return (
     <div className="App">
